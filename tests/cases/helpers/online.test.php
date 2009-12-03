4e035e553b00d8bd0cc6500bc2fdfda3c4c69373
webtechnick <nick@webtechnick.com>
2009-12-03 00:18:29 +0000

    Adding tests to new online plugin.

---
+++ tests/cases/helpers/online.test.php
@@ -0,0 +1,29 @@
+<?php
+App::import('Helper','Online.Online');
+
+class OnlineHelperTestCase extends CakeTestCase {
+  var $fixtures = array('plugin.online.online');
+  
+  function setUp(){
+    $this->online = new OnlineHelper();
+  }
+  
+  function testAll(){
+    $results = $this->online->all();
+    $this->assertTrue(!empty($results));
+        
+    $expected = array('0' => array(
+      'Online' => array(
+        'ip'  => 123456789,
+        'url'  => '/home',
+        'modified' => '2009-11-30 15:25:04'
+      )
+    ));
+		$this->assertEqual($results, $expected);
+  }
+  
+  function tearDown(){
+    unset($this->online);
+  }
+}
+?>
---
+++ tests/cases/models/online.test.php
@@ -0,0 +1,56 @@
+<?php
+App::import('Model','Online.Online');
+
+class OnlineTestCase extends CakeTestCase {
+  var $Online = null;
+  var $fixtures = array(
+    'plugin.online.online'
+  );
+  
+  function startTest(){
+    $this->Online = ClassRegistry::init('Online');
+    $this->Online->recursive = -1;
+  }
+  
+  function testOnlineInstance(){
+    $this->assertTrue(is_a($this->Online,'Online'));
+  }
+  
+  function testOnlineFind() {
+		$results = $this->Online->find('first');
+		$this->assertTrue(!empty($results));
+
+		$expected = array('Online' => array(
+			'ip'  => 123456789,
+      'url'  => '/home',
+      'modified' => '2009-11-30 15:25:04'
+	  ));
+		$this->assertEqual($results, $expected);
+	}
+	
+	function testUpdateUser(){
+	  $_SERVER['REMOTE_ADDR'] = '127.0.0.1';
+	  $this->Online->updateUser('/new/path.xml');
+	  
+	  $results = $this->Online->find('first', array('order' => 'Online.modified DESC'));
+	  $this->assertEqual($results['Online']['url'], '/new/path.xml');
+	}
+	
+	function testIpToNumber(){
+	  $this->assertEqual($this->Online->_ipAddressToNumber('127.0.0.1'), 2130706433);
+	}
+	
+	function testDeleteOld(){
+	  $results = $this->Online->find('first');
+	  $this->assertTrue(!empty($results));
+	  $this->Online->deleteOld();
+	  $results = $this->Online->find('first');
+	  $this->assertTrue(empty($results));
+	}
+	
+	function endTest(){
+	  ClassRegistry::flush();
+	}
+  
+}
+?>
